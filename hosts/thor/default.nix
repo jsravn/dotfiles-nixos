fc039c67385765249ddf8d24f5150d1d32f8a69d
James Ravn <james@r-vn.org>
2020-05-21 23:22:40 +0100

    Add initial thor configuration.

---
+++ hosts/thor/default.nix
@@ -0,0 +1,67 @@
+# Thor is my desktop machine.
+
+{ ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # General configuration
+  time.timeZone = "Europe/London";
+
+  # Modules
+  modules = {
+    desktop = {
+      apps.dropbox.enable = true;
+      #apps.evolution.enable = true;
+      apps.gnome-utils.enable = true;
+      apps.keybase.enable = true;
+      apps.sonarworks.enable = true;
+      apps.spotify.enable = true;
+
+      browsers.default = "chromium";
+      browsers.chromium.enable = true;
+
+      sway.enable = true;
+
+      term.default = "kitty";
+      term.kitty.enable = true;
+    };
+
+    dev = {
+      go.enable = true;
+    };
+
+    editors = {
+      default = "vim";
+      emacs.enable = true;
+      vim.enable = true;
+    };
+
+    security = {
+      # make-linux-fast-again.com
+      mitigations.disable = true;
+      mitigations.acceptRisk = true;
+    };
+
+    services = {
+      docker.enable = true;
+    };
+
+    shell = {
+      chezmoi.enable = true;
+      direnv.enable = true;
+      git.enable = true;
+      gpg.enable = true;
+      isync.enable = true;
+      keybase.enable = true;
+      mu.enable = true;
+      scmpuff.enable = true;
+      zsh.enable = true;
+    };
+
+    work = {
+      sky.enable = true;
+    };
+  };
+}
