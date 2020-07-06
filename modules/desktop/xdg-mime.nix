ef75e4012d9d4f6a4553970946398a8a9e7c62af
James Ravn <james@r-vn.org>
2020-07-06 11:27:51 +0100

    Update mimetypes, always use ssh for github for private repos

--- modules/desktop/xdg-mime.nix
+++ modules/desktop/xdg-mime.nix
@@ -3,7 +3,20 @@ with lib; {
   config = mkIf config.modules.desktop.enable {
     # Default applications and xdg-open.
     # See https://wiki.archlinux.org/index.php/XDG_MIME_Applications for details.
-    xdg.mime.enable = true;
-    my.packages = with pkgs; [ xdg_utils ];
+    my = {
+      packages = with pkgs; [ xdg_utils desktop-file-utils ];
+      home.xdg.mime.enable = true;
+      home.xdg.mimeApps = {
+        enable = true;
+        defaultApplications = {
+          "application/pdf" = [ "org.gnome.Evince.desktop" ];
+          "x-scheme-handler/http" = [ "chromium-browser.desktop" ];
+          "x-scheme-handler/https" = [ "chromium-browser.desktop" ];
+          "x-scheme-handler/slack" = [ "slack.desktop" ];
+          "text/plain" = [ "org.gnome.gedit.desktop" ];
+          "image/jpeg" = [ "eog.desktop" ];
+        };
+      };
+    };
   };
 }
