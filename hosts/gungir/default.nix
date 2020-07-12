00a115404ed501fc5bfe082f9daddef454d11b8d
James Ravn <james@r-vn.org>
2020-07-12 20:19:52 +0100

    Update gungir

--- hosts/gungir/default.nix
+++ hosts/gungir/default.nix
@@ -11,50 +11,36 @@
   # Modules
   modules = {
     desktop = {
-      apps.dropbox.enable = true;
-      #apps.evolution.enable = true;
-      apps.gnome-utils.enable = true;
-      apps.keybase.enable = true;
-      apps.sonarworks.enable = true;
-      apps.spotify.enable = true;
-
+      enable = true;
       browsers.default = "chromium";
-      browsers.chromium.enable = true;
-
-      sway.enable = true;
-
-      term.default = "kitty";
-      term.kitty.enable = true;
     };
 
-    dev = { go.enable = true; };
+    dev = {
+      # cc.enable = true;
+      go.enable = true;
+      node.enable = true;
+    };
 
     editors = {
       default = "vim";
       emacs.enable = true;
+      intellij.enable = true;
       vim.enable = true;
     };
 
-    security = {
-      # make-linux-fast-again.com
-      mitigations.disable = true;
-      mitigations.acceptRisk = true;
+    # make-linux-fast-again.com
+    security.mitigations.disable = true;
+    security.mitigations.acceptRisk = true;
+
+    services = {
+      docker.enable = true;
+      libvirtd.enable = true;
+      mullvad.enable = true;
+      printers.enable = true;
     };
 
-    services = { docker.enable = true; };
+    shell.enable = true;
 
-    shell = {
-      chezmoi.enable = true;
-      direnv.enable = true;
-      git.enable = true;
-      gpg.enable = true;
-      isync.enable = true;
-      keybase.enable = true;
-      mu.enable = true;
-      scmpuff.enable = true;
-      zsh.enable = true;
-    };
-
-    work = { sky.enable = true; };
+    work.sky.enable = true;
   };
 }
