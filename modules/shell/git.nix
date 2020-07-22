9eb3dc876e2baa380a8c265dc055dddfeb14c8a1
James Ravn <james@r-vn.org>
2020-07-22 13:01:30 +0100

    Move git config into chezmoi

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -3,11 +3,6 @@ with lib; {
   config = mkIf config.modules.shell.enable {
     my = {
       packages = with pkgs; [ gitAndTools.hub gitAndTools.diff-so-fancy ];
-      # Do recursively, in case git stores files in this folder
-      home.xdg.configFile = {
-        "git/config".source = <config/git/config>;
-        "git/ignore".source = <config/git/ignore>;
-      };
     };
   };
 }
