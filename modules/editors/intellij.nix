330bc949a4c2de6ac30e9829c38094ad5c507f17
James Ravn <james@r-vn.org>
2020-07-22 13:03:56 +0100

    Move IntelliJ files into chezmoi

--- modules/editors/intellij.nix
+++ modules/editors/intellij.nix
@@ -10,10 +10,6 @@ with lib; {
   config = mkIf config.modules.editors.intellij.enable {
     my = {
       packages = with pkgs.unstable.jetbrains; [ idea-ultimate ];
-      home.xdg.configFile."JetBrains/IntelliJIdea2020.1" = {
-        source = <config/intellij>;
-        recursive = true;
-      };
       # Provide some JDKs that IntelliJ can use (it doesn't support direnv, unfortunately).
       home.xdg.dataFile = with pkgs; {
         "jdk8".source = unstable.jdk8;
