d3e934dcabd3305c9758ed47120247d80571eff9
James Ravn <james@r-vn.org>
2020-07-24 10:25:28 +0100

    Move kitty.conf back into home-manager

--- modules/term/kitty.nix
+++ modules/term/kitty.nix
@@ -8,6 +8,13 @@ with lib; {
   };
 
   config = mkIf config.modules.term.kitty.enable {
-    my = { packages = with pkgs; [ kitty ]; };
+    my = {
+      packages = with pkgs; [ kitty ];
+
+      # Kitty config.
+      home.xdg.configFile."kitty" = {
+        source = <config/kitty>;
+      };
+    };
   };
 }
