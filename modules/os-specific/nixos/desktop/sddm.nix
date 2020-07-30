bd2088c4d0e1a279e6d5178f61f8d3f1e05d566b
James Ravn <james@r-vn.org>
2020-07-30 10:58:02 +0100

    Add theme dependencies

--- modules/os-specific/nixos/desktop/sddm.nix
+++ modules/os-specific/nixos/desktop/sddm.nix
@@ -2,7 +2,12 @@
 { config, lib, pkgs, ... }:
 with lib; {
   config = mkIf config.modules.desktop.enable {
-    environment.systemPackages = with pkgs; [ my.sddm-themes.sddm-sugar-dark ];
+    environment.systemPackages = with pkgs; [
+      my.sddm-themes.sddm-sugar-dark
+      qt5.qtgraphicaleffects
+      qt5.qtquickcontrols2
+      qt5.qtsvg
+    ];
     services.xserver.enable = true;
     services.xserver.displayManager = {
       sddm.enable = true;
