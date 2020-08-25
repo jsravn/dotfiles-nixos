c5633392ff05e596e112fdaf19b9f6a30de9e079
James Ravn <james@r-vn.org>
2020-08-25 12:05:03 +0100

    Tons of OSX improvements

--- modules/os-specific/darwin/default.nix
+++ modules/os-specific/darwin/default.nix
@@ -48,32 +48,151 @@
     });
 
     system.activationScripts.applications.text = pkgs.lib.mkForce (''
-      echo "setting up ~/Applications/NixApps..."
-      mkdir -p ~/Applications
-      rm -rf ~/Applications/NixApps
-      mkdir -p ~/Applications/NixApps
-      chown ${config.my.username} ~/Applications/NixApps
+      echo "setting up ~/Applications/Nix..."
+      rm -rf ~/Applications/Nix
+      mkdir -p ~/Applications/Nix
+      chown ${config.my.username} ~/Applications/Nix
       find ${config.system.build.applications}/Applications -maxdepth 1 -type l | while read f; do
         echo "Linking $f"
-        src=$(/usr/bin/stat -f%Y $f)
-        osascript -e "tell app \"Finder\" to make alias file at POSIX file \"/Users/${config.my.username}/Applications/NixApps/\" to POSIX file \"$src\"";
+        src="$(/usr/bin/stat -f%Y $f)"
+        osascript -e "tell app \"Finder\" to make alias file at POSIX file \"/Users/${config.my.username}/Applications/Nix/\" to POSIX file \"$src\" with properties {name: \"$appname\"}";
       done
     '');
 
-    # OS Settings.
+    # OSX Settings.
     system.defaults = {
-      dock = { autohide = true; };
+      # Dock settings.
+      dock = {
+        # Autohide and remove the delay.
+        autohide = true;
+        autohide-delay = "0.0";
+        autohide-time-modifier = "0.0";
+
+        # Put on left of screen.
+        orientation = "left";
+
+        # Enable highlight hover effect.
+        mouse-over-hilite-stack = true;
+
+        # Default is 64.
+        tilesize = 36;
+
+        # Minimize effect.
+        mineffect = "scale";
+
+        # Minimize things to the application they are associated with.
+        minimize-to-application = true;
+
+        # Show indicator lights for open applications in the dock.
+        show-process-indicators = true;
+
+        # Reduce expose animation.
+        expose-animation-duration = "0.1";
+
+        # Don't animate opening applications from the dock.
+        launchanim = false;
+
+        # Don't group by application in Mission Control.
+        expose-group-by-app = false;
+
+        # Don't show dashboard as a space.
+        dashboard-in-overlay = true;
+
+        # Don't rearrange spaces automatically.
+        mru-spaces = false;
+
+        # Don't show recent applications in dock.
+        show-recents = false;
+
+        # Make hidden application icons translucent.
+        showhidden = true;
+      };
+
+      # Enable tap to click.
+      trackpad.Clicking = true;
 
       finder = {
+        # Show all extensions.
         AppleShowAllExtensions = true;
         _FXShowPosixPathInTitle = true;
         FXEnableExtensionChangeWarning = false;
+
+        # Allow quitting finder.
+        QuitMenuItem = true;
       };
 
       NSGlobalDomain = {
-        InitialKeyRepeat = 15;
-        KeyRepeat = 2;
+        # Key rate.
+        InitialKeyRepeat = 10;
+        KeyRepeat = 1;
+
+        # Improve font smooth on non-retina displays (https://sayzlim.net/font-smoothing-non-retina-display/).
+        AppleFontSmoothing = 1;
+
+        # Always show the scrollbars.
+        AppleShowScrollBars = "Always";
+
+        # Sidebar icon size to medium.
+        NSTableViewDefaultSizeMode = 2;
+
+        # Disable over the top focus animation.
+        NSUseAnimatedFocusRing = false;
+
+        # Speed up resize animation.
+        NSWindowResizeTime = "0.001";
+
+        # Expand save menus by default.
+        NSNavPanelExpandedStateForSaveMode = true;
+        NSNavPanelExpandedStateForSaveMode2 = true;
+
+        # Expand print menus by default.
+        PMPrintingExpandedStateForPrint = true;
+        PMPrintingExpandedStateForPrint2 = true;
+
+        # Save to disk by default, not iCloud :facepalm:.
+        NSDocumentSaveNewDocumentsToCloud = false;
+
+        # Display ASCII control characters using caret notation in standard text views.
+        NSTextShowsControlCharacters = true;
+
+        # Disable automatic termination of inactive apps - this isn't iOS goddammit.
+        NSDisableAutomaticTermination = true;
+
+        # Disable automatic capitalization.
+        NSAutomaticCapitalizationEnabled = false;
+
+        # Disable automatic dash substitution.
+        NSAutomaticDashSubstitutionEnabled = false;
+
+        # Disable "smart" period.
+        NSAutomaticPeriodSubstitutionEnabled = false;
+
+        # Disable "smart" quotation.
+        NSAutomaticQuoteSubstitutionEnabled = false;
+
+        # Disable automatic spelling correction.
+        NSAutomaticSpellingCorrectionEnabled = false;
+
+        # Disable inverse mouse scrolling.
+        "com.apple.swipescrolldirection" = false;
+
+        # Enable full keyboard access in all modes.
+        AppleKeyboardUIMode = 3;
+
+        # Disable press and hold - let us repeat by holding down keys.
+        ApplePressAndHoldEnabled = false;
+
+        # Enable expose for directories.
+        "com.apple.springing.enabled" = true;
+        "com.apple.springing.delay" = "0.0";
+ 
+        # Set locale information. Not everything is available here - see the activation script below.
+        AppleMeasurementUnits = "Centimeters";
+        AppleMetricUnits = 1;
       };
+
+      # Disable "Are you sure?" dialogue when running downloaded applications.
+      LaunchServices.LSQuarantine = false;
     };
 
     system.keyboard = {
@@ -81,6 +200,249 @@
       remapCapsLockToControl = true;
       nonUS.remapTilde = true;
     };
+
+    # Set things not in the nix-darwin module.
+    # See https://github.com/mathiasbynens/dotfiles/blob/main/.macos for a reference of everything possible.
+    # These should all be moved into the nix-darwin module at some point.
+    system.activationScripts.extraActivation.text = ''
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Disable transparency in the menu bar and elsewhere on Yosemite
+defaults write com.apple.universalaccess reduceTransparency -bool true
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Remove duplicates in the “Open With” menu (also see `lscleanup` alias)
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+
+# Disable Resume system-wide
+defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
+
+# Set Help Viewer windows to non-floating mode
+defaults write com.apple.helpviewer DevMode -bool true
+
+# Reveal IP address, hostname, OS version, etc. when clicking the clock
+# in the login window
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+# Increase sound quality for Bluetooth headphones/headsets
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+
+# Use scroll gesture with the Ctrl (^) modifier key to zoom
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
+# Follow the keyboard focus while zoomed in
+defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
+
+# Set language and text formats
+# Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
+# `Inches`, `en_GB` with `en_US`, and `true` with `false`.
+defaults write NSGlobalDomain AppleLanguages -array "en-GB" "en"
+defaults write NSGlobalDomain AppleLocale -string "en_GB@currency=GBP"
+
+# Show language menu in the top right corner of the boot screen
+sudo defaults write /Library/Preferences/com.apple.loginwindow showInputMenu -bool true
+
+###############################################################################
+# Energy saving                                                               #
+###############################################################################
+
+## These power settings supposedly fix a lot of the problems w/ the macbook not sleeping properly.
+# Enable lid wakeup
+sudo pmset -a lidwake 1
+
+# Restart automatically on power loss
+sudo pmset -a autorestart 1
+
+# Restart automatically if the computer freezes
+sudo systemsetup -setrestartfreeze on
+
+# Sleep the display after 15 minutes
+sudo pmset -a displaysleep 15
+
+# Disable machine sleep while charging
+sudo pmset -c sleep 0
+
+# Set machine sleep to 5 minutes on battery
+sudo pmset -b sleep 5
+
+# Set standby delay to 24 hours (default is 1 hour)
+sudo pmset -a standbydelay 86400
+
+# Never go into computer sleep mode
+sudo systemsetup -setcomputersleep Off > /dev/null
+
+# Hibernation mode
+# 0: Disable hibernation (speeds up entering sleep mode)
+# 3: Copy RAM to disk so the system state can still be restored in case of a
+#    power failure.
+sudo pmset -a hibernatemode 0
+
+# Remove the sleep image file to save disk space
+# sudo rm /private/var/vm/sleepimage
+# Create a zero-byte file instead…
+# sudo touch /private/var/vm/sleepimage
+# …and make sure it can’t be rewritten
+# sudo chflags uchg /private/var/vm/sleepimage
+
+###############################################################################
+# Screen                                                                      #
+###############################################################################
+
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Save screenshots to the desktop
+defaults write com.apple.screencapture location -string "/Users/${config.my.username}/Desktop"
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+
+# Enable HiDPI display modes (requires restart)
+sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
+
+###############################################################################
+# Finder                                                                      #
+###############################################################################
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Disable disk image verification
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+
+# Disable disk image verification
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+# Show item info near icons on the desktop and in other icon views
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+
+# Show item info to the right of the icons on the desktop
+/usr/libexec/PlistBuddy -c "Set DesktopViewSettings:IconViewSettings:labelOnBottom false" ~/Library/Preferences/com.apple.finder.plist
+
+# Enable snap-to-grid for icons on the desktop and in other icon views
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+# Increase grid spacing for icons on the desktop and in other icon views
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
+
+# Increase the size of icons on the desktop and in other icon views
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:iconSize 80" ~/Library/Preferences/com.apple.finder.plist
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Enable AirDrop over Ethernet and on unsupported Macs running Lion
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+# Show the ~/Library folder
+chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
+
+# Show the /Volumes folder
+sudo chflags nohidden /Volumes
+
+# Remove Dropbox’s green checkmark icons in Finder
+file=/Applications/Dropbox.app/Contents/Resources/emblem-dropbox-uptodate.icns
+[ -e "''${file}" ] && mv -f "''${file}" "''${file}.bak"
+
+# Expand the following File Info panes:
+# “General”, “Open with”, and “Sharing & Permissions”
+defaults write com.apple.finder FXInfoPanesExpanded -dict \
+	General -bool true \
+	OpenWith -bool true \
+
+###############################################################################
+# Dock, Dashboard, and hot corners                                            #
+###############################################################################
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+# 13: Lock Screen
+# Top left screen corner → Mission Control
+defaults write com.apple.dock wvous-tl-corner -int 2
+defaults write com.apple.dock wvous-tl-modifier -int 0
+# Top right screen corner → Desktop
+defaults write com.apple.dock wvous-tr-corner -int 4
+defaults write com.apple.dock wvous-tr-modifier -int 0
+# Bottom left screen corner → Start screen saver
+defaults write com.apple.dock wvous-bl-corner -int 5
+defaults write com.apple.dock wvous-bl-modifier -int 0
+
+###############################################################################
+# Google Chrome & Google Chrome Canary                                        #
+###############################################################################
+
+# Disable the all too sensitive backswipe on trackpads
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
+defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool false
+
+# Disable the all too sensitive backswipe on Magic Mouse
+defaults write com.google.Chrome AppleEnableMouseSwipeNavigateWithScrolls -bool false
+defaults write com.google.Chrome.canary AppleEnableMouseSwipeNavigateWithScrolls -bool false
+
+# Use the system-native print preview dialog
+defaults write com.google.Chrome DisablePrintPreview -bool true
+defaults write com.google.Chrome.canary DisablePrintPreview -bool true
+
+# Expand the print dialog by default
+defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
+defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
+    '';
 
     # Use pinentry for gpg-agent.
     # HACK Without this config file you get "No pinentry program" on 20.03.
