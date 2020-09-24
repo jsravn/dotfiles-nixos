293cb8fd5060fe0af32ffaa123d66f1c884ab295
James Ravn <james@r-vn.org>
2020-09-24 11:49:07 +0100

    Small refactor of sound.nix

--- modules/os-specific/nixos/desktop/sound.nix
+++ modules/os-specific/nixos/desktop/sound.nix
@@ -2,16 +2,19 @@
 with lib; {
   config = mkIf config.modules.desktop.enable {
     sound.enable = true;
-    hardware.pulseaudio.enable = true;
-    hardware.pulseaudio.support32Bit = true;
-    hardware.pulseaudio.daemon.config = {
-      # 5 is good - see https://gitlab.freedesktop.org/pulseaudio/pulseaudio/issues/310
-      resample-method = "speex-float-5";
-      flat-volumes = "no";
-      realtime-scheduling = "yes";
-      default-sample-format = "float32le";
-      default-sample-rate = "48000";
-      alternate-sample-rate = "44100";
+    hardware.pulseaudio = {
+      enable = true;
+      support32Bit = true;
+      package = pkgs.pulseaudioFull;  # Includes the JACK and bluetooth modules.
+      daemon.config = {
+        # 5 is good - see https://gitlab.freedesktop.org/pulseaudio/pulseaudio/issues/310
+        resample-method = "speex-float-5";
+        flat-volumes = "no";
+        realtime-scheduling = "yes";
+        default-sample-format = "float32le";
+        default-sample-rate = "48000";
+        alternate-sample-rate = "44100";
+      };
     };
     security.rtkit.enable = true;
     my.packages = with pkgs; [ pavucontrol ];
