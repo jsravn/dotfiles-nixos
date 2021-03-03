98a71f9ac228240354a85a748bd2dc4c91eb3360
James Ravn <james@r-vn.org>
2021-03-03 17:07:53 +0000

    Add waifu2x

--- modules/os-specific/nixos/desktop/scanner.nix
+++ modules/os-specific/nixos/desktop/scanner.nix
@@ -3,5 +3,6 @@ with lib; {
   config = mkIf config.modules.desktop.enable {
     hardware.sane.enable = true;
     my.user.extraGroups = [ "scanner" "lp" ];
+    my.packages = with pkgs; [ waifu2x-converter-cpp ];
   };
 }
