ebb9c3aa97bd5f7cae11e1db2f5e72f3e917799e
James Ravn <james@r-vn.org>
2021-09-14 11:41:24 +0100

    Remove legacy pulseeffects code

--- modules/desktop/sound.nix
+++ modules/desktop/sound.nix
@@ -1,25 +1,6 @@
 { config, lib, pkgs, ... }:
 with lib; {
   config = mkIf config.modules.desktop.enable {
-    # Enable alsa.
-   # sound.enable = true;
-    # hardware.pulseaudio = {
-    #   enable = true;
-    #   support32Bit = true;
-    #   package = pkgs.pulseaudioFull; # Includes the JACK and bluetooth modules.
-    #   daemon.config = {
-    #     # 5 is good - see https://gitlab.freedesktop.org/pulseaudio/pulseaudio/issues/310
-    #     resample-method = "speex-float-5";
-    #     flat-volumes = "no";
-    #     realtime-scheduling = "yes";
-    #     default-sample-format = "float32le";
-    #     default-sample-rate = "48000";
-    #     alternate-sample-rate = "44100";
-    #     # Just set DAC to the match the rate when possible.
-    #     avoid-resampling = "yes";
-    #   };
-    # };
-
     services.pipewire = {
       enable = true;
       alsa.enable = true;
@@ -34,11 +15,8 @@ with lib; {
 
     environment.systemPackages = with pkgs; [
       alsaUtils
-      pulseeffects-pw
       pavucontrol
-      #pulseeffects-legacy
-      # my.dsp
-      # pulseeffects-pw
+      unstable.easyeffects
     ];
   };
 }
