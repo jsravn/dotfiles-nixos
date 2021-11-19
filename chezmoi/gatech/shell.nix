d0f86fb8243669006201ebc8a52ee92159c53b59
James Ravn <james@r-vn.org>
2021-11-19 14:37:04 +0000

    Update chezmoi files

--- chezmoi/gatech/shell.nix
+++ chezmoi/gatech/shell.nix
@@ -1,31 +1,27 @@
 let
-  pkgs = import <nixpkgs> { config.allowUnfree = true; };
-  myjava = pkgs.jdk8;
+  pkgs = import <nixos> {};
+  unstable = import <nixpkgs-unstable> {};
 in
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    gcc
-    clang
-    bear
-    zip
-    gnumake
-    gdb
-    cudatoolkit
-    linuxPackages.nvidia_x11
-    libGLU libGL
-    xorg.libXi xorg.libXmu freeglut
-    xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib
-    ncurses5 stdenv.cc binutils
-    openmpi
-    gnuplot
-    myjava
-  ];
-  shellHook = ''
-      export CUDA_PATH=${pkgs.cudatoolkit}
-      export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib:${pkgs.cudatoolkit}/extras/CUPTI/lib64
-      export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
-      export EXTRA_CCFLAGS="-I/usr/include"
-      export JAVA_HOME=${myjava}
-      export PATH="${myjava}/bin:$PATH"
-  '';
-}
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+      # to execute 32-bit binaries easily with steam-run
+      steam-run-native
+      pwndbg
+      pwntools
+      tcpdump
+      zlib
+      mpack
+      hivex
+      jdk8
+      dos2unix
+      hexedit
+      xinetd
+
+      (pkgs.python3.withPackages (p: with p; [
+        pynacl
+        pwntools
+        joblib
+      ]))
+      libseccomp
+    ];
+  }
--- chezmoi/sky/shell.nix
+++ chezmoi/sky/shell.nix
@@ -1,16 +1,17 @@
 let
-  pkgs = import <nixpkgs-unstable> {};
-  my-python = pkgs.python3.withPackages (ps: with ps; [ boto3 black kubernetes ]);
+  pkgs = import <nixos> {};
+  unstable = import <nixpkgs-unstable> {};
+  my-python = pkgs.python3.withPackages (ps: with ps; [ boto3 black kubernetes j2cli ]);
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     gcc
     gnumake
     ansible
-    go
-    adoptopenjdk-bin
+    unstable.go_1_17
+    jdk11
     buildkit
-    kustomize
+    unstable.kustomize
     gradle
     jq
     keepassx2
@@ -20,7 +21,13 @@ pkgs.mkShell {
     prometheus
     openssl
     zip
-    terraform
+    unstable.terraform
     python-language-server
+    unstable.google-cloud-sdk
+    unstable.tektoncd-cli
+    unstable.stern
   ];
+  shellHook = ''
+    export JAVA_HOME=${pkgs.jdk11}/lib/openjdk
+  '';
 }
