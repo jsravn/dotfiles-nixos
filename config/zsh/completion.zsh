29be570bc8db96d3a4b2ee4225548dcb19028c05
James Ravn <james@r-vn.org>
2021-12-04 19:05:44 +0000

    path dirs is too bloody slow

--- config/zsh/completion.zsh
+++ config/zsh/completion.zsh
@@ -1,11 +1,11 @@
 ## Options
 setopt COMPLETE_IN_WORD    # Complete from both ends of a word.
-setopt PATH_DIRS           # Perform path search even on command names with slashes.
 setopt AUTO_MENU           # Show completion menu on a successive tab press.
 setopt AUTO_LIST           # Automatically list choices on ambiguous completion.
 setopt AUTO_PARAM_SLASH    # If completed parameter is a directory, add a trailing slash.
 setopt AUTO_PARAM_KEYS
 setopt FLOW_CONTROL        # Disable start/stop characters in shell editor.
+unsetopt PATH_DIRS           # Perform path search even on command names with slashes.
 unsetopt MENU_COMPLETE     # Do not autoselect the first completion entry.
 unsetopt COMPLETE_ALIASES  # Completion for aliases
 unsetopt ALWAYS_TO_END     # Move cursor to the end of a completed word.
